31aa843772dcef1c8205f5be9fd6466504647e16
jdm080620 <jdm080620@gmail.com>
2024-04-22 13:48:22 +0900

    Chore: 프론트엔드 pwa 설정 - manifest 설정 추가 - favicon 및 아이콘 파일 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+      manifest: {
+        theme_color: "#ca965c",
+        background_color: "#f3eeea",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "What's your plan",
+        short_name: "wypl",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "/icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/icon-256.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon-512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
+});
