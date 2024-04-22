766382d6b5e508922d8ffe71d72e22c8aea0859b
KIMSEI1124 <workju1124@gmail.com>
2024-04-22 14:07:42 +0900

    Chore: Init Project

---
+++ backend/src/main/java/com/butter/wypl/WyplApplication.java
@@ -0,0 +1,13 @@
+package com.butter.wypl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyplApplication.class, args);
+	}
+
+}
