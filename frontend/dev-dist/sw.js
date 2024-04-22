6f0aaaccbf99a4dbbcb92eadf7df24aed37d0a7b
jdm080620 <jdm080620@gmail.com>
2024-04-22 15:14:28 +0900

    Chore: index.html 헤드 수정(S10P31A602-257) - favicon, title 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c676b6d3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l4h9vctd81"
+    "revision": "0.2ct38f6qde8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
