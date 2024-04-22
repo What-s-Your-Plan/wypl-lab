64cc0d0a44f1bf7f6d9c4a8d85060bce34214a0e
jdm080620 <jdm080620@gmail.com>
2024-04-22 14:41:25 +0900

    Chore: FE 프로젝트 eslint 설정(S10P31A602-209) - import order 추가

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,17 +2,75 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "import",
+    "react",
+    "react-hooks",
+    "react-refresh",
+    "@typescript-eslint",
+    "prettier",
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["sibling", "parent", "index"],
+          "type",
+          "unknown",
+        ],
+        pathGroups: [
+          {
+            pattern: "react*",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@/api/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@/utils/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@/pages/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@/components/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@/assets/*",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react", "unknown"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
-}
+};
