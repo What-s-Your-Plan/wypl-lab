0b55dc03933c1c105699294e8eed52f3536ebbba
jdm080620 <jdm080620@gmail.com>
2024-04-22 14:55:41 +0900

    Chore: 초기 폴더 구조 설정(S10P31A602-256) - 폴더 및 gitkeep 파일 추가 - prettierrc 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,46 +1,46 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
       },
-      injectRegister: "auto",
+      injectRegister: 'auto',
       manifest: {
-        theme_color: "#ca965c",
-        background_color: "#f3eeea",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
+        theme_color: '#ca965c',
+        background_color: '#f3eeea',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
         name: "What's your plan",
-        short_name: "wypl",
+        short_name: 'wypl',
         icons: [
           {
-            src: "favicon.ico",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon",
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
           },
           {
-            src: "/icon-192.png",
-            type: "image/png",
-            sizes: "192x192",
+            src: '/icon-192.png',
+            type: 'image/png',
+            sizes: '192x192',
           },
           {
-            src: "/icon-256.png",
-            type: "image/png",
-            sizes: "192x192",
-            purpose: "maskable",
+            src: '/icon-256.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable',
           },
           {
-            src: "/icon-512.png",
-            type: "image/png",
-            sizes: "512x512",
+            src: '/icon-512.png',
+            type: 'image/png',
+            sizes: '512x512',
           },
         ],
       },
