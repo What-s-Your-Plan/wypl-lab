8f601d955ac31db532226eaa4a6fc0a6d2ddb54e
KIMSEI1124 <workju1124@gmail.com>
2024-04-23 10:36:42 +0900

    Refactor: MemberRepositoryTest 코드 분리(S10P31A602-258)
    BaseEntity 관련 테스트 코드 분리

--- backend/src/test/java/com/butter/wypl/member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/butter/wypl/member/repository/MemberRepositoryTest.java
@@ -1,10 +1,8 @@
 package com.butter.wypl.member.repository;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,38 +17,17 @@ class MemberRepositoryTest {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	@Nested
-	@DisplayName("회원 저장 테스트")
-	class saveTest {
-		@DisplayName("회원을 데이터베이스에 저장한다.")
-		@ParameterizedTest
-		@EnumSource(value = MemberFixture.class)
-		void saveByMemberTest(MemberFixture memberFixture) {
-			/* Given */
-			Member member = memberFixture.toMember();
+	@DisplayName("회원을 데이터베이스에 저장한다.")
+	@ParameterizedTest
+	@EnumSource(value = MemberFixture.class)
+	void saveByMemberTest(MemberFixture memberFixture) {
+		/* Given */
+		Member member = memberFixture.toMember();
 
-			/* When */
-			/* Then */
-			assertThatCode(() -> {
-				memberRepository.save(member);
-			}).doesNotThrowAnyException();
-		}
-
-		@DisplayName("저장한 회원의 생성 시간은 존재한다.")
-		@ParameterizedTest
-		@EnumSource(value = MemberFixture.class)
-		void existedByMemberCreatedAt(MemberFixture memberFixture) {
-			/* Given */
-			Member member = memberFixture.toMember();
-
-			/* When */
-			Member savedMember = memberRepository.save(member);
-
-			/* Then */
-			assertAll(
-					() -> assertThat(savedMember.getCreatedAt()).isNotNull(),
-					() -> assertThat(savedMember.getModifiedAt()).isNotNull()
-			);
-		}
+		/* When */
+		/* Then */
+		assertThatCode(() -> {
+			memberRepository.save(member);
+		}).doesNotThrowAnyException();
 	}
 }
