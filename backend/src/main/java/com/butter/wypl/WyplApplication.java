8a379c5f8f1469db1ca975a05ccd09bcca3e2f7b
KIMSEI1124 <workju1124@gmail.com>
2024-04-23 17:24:46 +0900

    Chore: Jwt Properties 설정 (S10P31A602-266)
    Custom Properties 설정

--- backend/src/main/java/com/butter/wypl/WyplApplication.java
+++ backend/src/main/java/com/butter/wypl/WyplApplication.java
@@ -2,7 +2,9 @@ package com.butter.wypl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class WyplApplication {
 
