68250bdbf2e9ab18abb699e8392c2a181d258d6b
KIMSEI1124 <workju1124@gmail.com>
2024-04-23 16:14:51 +0900

    Fix: MemberException 수정 (S10P31A602-261)
    ErrorCode를 MemberErrorCode로 수정

--- backend/src/main/java/com/butter/wypl/member/exception/MemberException.java
+++ backend/src/main/java/com/butter/wypl/member/exception/MemberException.java
@@ -1,10 +1,9 @@
 package com.butter.wypl.member.exception;
 
 import com.butter.wypl.global.exception.CustomException;
-import com.butter.wypl.global.exception.ErrorCode;
 
 public class MemberException extends CustomException {
-	public MemberException(ErrorCode errorCode) {
+	public MemberException(MemberErrorCode errorCode) {
 		super(errorCode);
 	}
 }
