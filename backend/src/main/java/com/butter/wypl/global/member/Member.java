aa3239015e6b38951b299b65b5b1686cda1186ab
KIMSEI1124 <workju1124@gmail.com>
2024-04-23 09:12:17 +0900

    Feat: Member Entity 작성(S10P31A602-258)

---
+++ backend/src/main/java/com/butter/wypl/global/member/Member.java
@@ -0,0 +1,40 @@
+package com.butter.wypl.global.member;
+
+import java.time.LocalDate;
+
+import com.butter.wypl.global.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private int id;
+
+	@Column(name = "birthday")
+	private LocalDate birthday;
+
+	@Column(name = "nickname", length = 10, nullable = false)
+	private String nickname;
+
+	@Column(name = "profile_image", length = 100)
+	private String profileImage;
+
+	@Column(name = "color", length = 6, nullable = false)
+	private String color;
+}
