a2db1c2c142d12db1843be5867686ee4e8ae479b
KIMSEI1124 <workju1124@gmail.com>
2024-04-23 10:24:59 +0900

    Feat: JpaRepositoryTest 어노테이션 생성(S10P31A602-258)

---
+++ backend/src/test/java/com/butter/wypl/global/annotation/JpaRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.butter.wypl.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.butter.wypl.global.config.JpaAuditingConfiguration;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(JpaAuditingConfiguration.class)
+@DataJpaTest
+public @interface JpaRepositoryTest {
+}
