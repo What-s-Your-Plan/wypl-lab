ab94a566764db762733bab89fb2ee091787adcae
KIMSEI1124 <workju1124@gmail.com>
2024-04-24 10:04:35 +0900

    Feat: Embedded Redis 추가 (S10P31A602-266)

--- backend/src/test/java/com/butter/wypl/auth/utils/JwtProviderTest.java
+++ backend/src/test/java/com/butter/wypl/auth/utils/JwtProviderTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.butter.wypl.auth.data.JsonWebTokens;
+import com.butter.wypl.global.annotation.ServiceTest;
 
-@SpringBootTest
+@ServiceTest
 class JwtProviderTest {
 
 	@Autowired
