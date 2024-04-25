61a8c6b75fd07b1e31a84ea465d17c4c8da6d2ed
Jiwon Lee <leeji7031@gmail.com>
2024-04-25 09:34:58 +0900

    Feat: 공통 컴포넌트 모달(S10P31A602-218)

---
+++ frontend/src/components/common/modal/Modal.tsx
@@ -0,0 +1,104 @@
+import { Dialog, Transition } from '@headlessui/react';
+import React, { Fragment } from 'react';
+import Button from '@/components/common/Button';
+
+type Props = {
+  isOpen: boolean;
+  cancel?: string;
+  confirm?: string;
+  title: React.ReactNode;
+  contents: React.ReactNode;
+  handleClose: (() => void) | (() => Promise<void>);
+  handleConfirm?: (() => void) | (() => Promise<void>);
+};
+
+function Modal({
+  isOpen,
+  cancel = '닫기',
+  confirm,
+  title,
+  contents,
+  handleClose,
+  handleConfirm,
+}: Props) {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => {
+            handleClose();
+          }}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title as="div" className="max-h-[10vh]">
+                    {/* 타이틀 */}
+                    {title}
+                  </Dialog.Title>
+                  <div className="mt-2 max-h-[70vh] overflow-auto">
+                    {/* 컨텐츠 */}
+                    {contents}
+                  </div>
+                  <div className="mt-4 flex justify-end gap-3">
+                    <Button
+                      $size="lg"
+                      $width="60px"
+                      $bgColor="white"
+                      $textColor="black"
+                      $border="black"
+                      onClick={() => {
+                        handleClose();
+                      }}
+                    >
+                      {cancel}
+                    </Button>
+                    {confirm ? (
+                      <Button
+                        $size="lg"
+                        $width="60px"
+                        $bgColor="labelBrown"
+                        $textColor="white"
+                        onClick={() => {
+                          handleConfirm ? handleConfirm() : null;
+                          handleClose();
+                        }}
+                      >
+                        {confirm}
+                      </Button>
+                    ) : null}
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
+
+export default Modal;
