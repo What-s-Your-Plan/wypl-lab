c361a85c6c9b386c2833f6186e47a54dc7c81ffc
Jiwon Lee <leeji7031@gmail.com>
2024-04-25 14:59:16 +0900

    Feat: Input $width, $isValid props 추가

--- frontend/src/components/common/InputText.ts
+++ frontend/src/components/common/InputText.ts
@@ -1,33 +1,38 @@
 import tw from 'twin.macro';
 import styled from 'styled-components';
 
-const InputDefault = styled.input`
-  ${
-    tw`
-    bg-gray-300
+type Props = {
+  $width?: string;
+  $isValid?: boolean;
+};
+
+const InputDefault = styled.input<Props>`
+  ${tw`
     rounded-md
     h-[32px]
-    grow
     px-2
     transition
     focus:outline-main
-    `
-  }
-`
+    `}
+  ${(props) => (props.$isValid === false ? tw`bg-label-pink` : tw`bg-gray-300`)}
+  ${(props) => (props.$width ? `width:${props.$width};` : tw`grow`)}
+`;
 
-const InputTitle = styled.input`
+const InputTitle = styled.input<Props>`
   ${tw`
       border-b-2
       border-gray-300 // 색상 변경 필요
+      font-bold
       h-[32px]
-      grow
       px-2
       transition
-      font-bold
       placeholder:text-gray-300
       focus:outline-none
       focus:border-main
   `}
+  ${(props) =>
+    props.$isValid === false ? tw`bg-label-pink` : tw`bg-transparent`}
+  ${(props) => (props.$width ? `width:${props.$width};` : tw`grow`)}
 `;
 
-export {InputDefault, InputTitle};
+export { InputDefault, InputTitle };
