e577d9ff6ebb74d8eb526f88f1ef8925eeba1cff
KIMSEI1124 <workju1124@gmail.com>
2024-04-29 09:52:23 +0900

    Chore: Logging 설정 수정 (S10P31A602-358)

--- backend/src/main/java/com/butter/wypl/global/common/interceptor/LoggingInterceptor.java
+++ backend/src/main/java/com/butter/wypl/global/common/interceptor/LoggingInterceptor.java
@@ -14,23 +14,30 @@ public class LoggingInterceptor implements HandlerInterceptor {
 	private static final ThreadLocal<Long> startTime = new ThreadLocal<>();
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
-		Exception {
-		log.info("요청 URL => {}", request.getRequestURL());
+	public boolean preHandle(
+			final HttpServletRequest request,
+			final HttpServletResponse response,
+			final Object handler
+	) throws Exception {
+		log.info("\n요청 URL => [{}]", request.getRequestURL());
 		startTime.set(System.currentTimeMillis());
-
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
 
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-		Exception ex) throws Exception {
+	public void afterCompletion(
+			final HttpServletRequest request,
+			final HttpServletResponse response,
+			final Object handler,
+			final Exception ex
+	) throws Exception {
 		long duration = System.currentTimeMillis() - startTime.get();
 		startTime.remove();
-
-		log.info("요청 URL => {}, status => {}, 처리 시간 => {}ms", request.getRequestURL(), response.getStatus(),
-			duration);
-
+		log.info("\n요청 URL => [{}], status => [{}], 처리 시간 => [{}ms]",
+				request.getRequestURL(),
+				response.getStatus(),
+				duration
+		);
 		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
 	}
 }
