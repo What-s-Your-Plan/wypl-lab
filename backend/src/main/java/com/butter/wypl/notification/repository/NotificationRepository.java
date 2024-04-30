58ce59b86dfece0ebfa0ca1c4c12eac0cdd08c51
최민준 <dailymaro@naver.com>
2024-04-30 15:49:09 +0900

    Feat : 알림 Repository

--- backend/src/main/java/com/butter/wypl/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/butter/wypl/notification/repository/NotificationRepository.java
@@ -1,9 +1,19 @@
 package com.butter.wypl.notification.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.butter.wypl.notification.domain.Notification;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
+	// 최초 조회, _id 없는 경우
+	@Query(value = "{memberId: ?0}", sort = "{_id: -1}")
+	List<Notification> findAllByMemberId(int memberId, Pageable pageable);
 
+	// 마지막행 ID 있는 경우
+	@Query(value = "{memberId: ?0, _id: {$lt: {$oid: ?1} }}", sort = "{_id: -1}")
+	List<Notification> findAllById(int memberId, String lastId, Pageable pageable);
 }
--- backend/src/test/java/com/butter/wypl/notification/repository/NotificationRepositoryTest.java
+++ backend/src/test/java/com/butter/wypl/notification/repository/NotificationRepositoryTest.java
@@ -10,6 +10,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 
 import com.butter.wypl.global.annotation.MongoRepositoryTest;
 import com.butter.wypl.notification.domain.Notification;
@@ -34,10 +35,60 @@ class NotificationRepositoryTest {
 	 *
 	 * 2. 알림 조회
 	 *  - 회원 ID로 조회, order by 생성일시 desc
+	 *  -
 	 * 3. 알림 삭제
+	 *  - 사용자 직접 삭제
 	 *  - 읽지 않은 알림 => 생성일로부터 30일 지나면 삭제
 	 *  - 읽은 알림 => 읽고 난 뒤 7일 뒤 삭제
 	 */
+
+	@Test
+	void 임시저장 () throws InterruptedException {
+		// given
+		String message = "";
+		String type = "Group";
+
+		for (int i = 0; i < 30; i++) {
+			List<NotificationButton> buttons = new ArrayList<>();
+			buttons.add(
+				NotificationButton.builder()
+					.text("수락")
+					.actionUrl("ok")
+					.color("#000000")
+					.logo("수락 로고" + i)
+					.build()
+			);
+			buttons.add(
+				NotificationButton.builder()
+					.text("취소")
+					.actionUrl("cancel")
+					.color("#000000")
+					.logo("버튼 로고" + i)
+					.build()
+			);
+
+			if (i % 2 == 0) {
+				message = "그룹초대메시지" + i;
+				type = "Group";
+			} else {
+				message = "회고작성메시지" + i;
+				type = "Review";
+			}
+
+			Thread.sleep(1000);
+
+			Notification notification = Notification.builder()
+				.memberId(99)
+				.message(message)
+				.buttons(buttons)
+				.isRead(false)
+				.type(type)
+				.build();
+
+			notificationRepository.save(notification);
+		}
+		// when
+	}
 
 	@Test
 	@DisplayName("특정 이벤트 발생 후 알림을 생성한다.")
@@ -62,7 +113,6 @@ class NotificationRepositoryTest {
 				.build()
 		);
 
-
 		String type = "Group";
 
 		Notification notification = Notification.builder()
@@ -81,4 +131,49 @@ class NotificationRepositoryTest {
 		assertThat(savedNotification.getMemberId()).isEqualTo(1);
 	}
 
+	@Test
+	@DisplayName("알림 최초 조회")
+	void findNotificationByMemberId () {
+	    //given
+		int memberId = 99;
+		PageRequest pageReq = PageRequest.of(0, 10);
+
+		//when
+		List<Notification> result = notificationRepository.findAllByMemberId(memberId, pageReq);
+		//then
+
+
+	}
+
+	@Test
+	@DisplayName("이후 알림 조회")
+	void findNotificationByMemberIdAndId () {
+	    //given
+		int memberId = 99;
+	    //when
+		PageRequest pageReq = PageRequest.of(0, 10);
+		String lastId = "6630481609e3d42813504c86";
+		List<Notification> result = notificationRepository.findAllById(memberId, lastId, pageReq);
+	    //then
+	}
+	
+	@Test
+	@DisplayName("사용자 알림 삭제")
+	void removeNotification () {
+	    //given
+	    
+	    //when
+	    
+	    //then
+	}
+
+	@Test
+	@DisplayName("배치 알림 삭제")
+	void removeNotificationBatch () {
+	    //given
+
+	    //when
+
+	    //then
+	}
 }
