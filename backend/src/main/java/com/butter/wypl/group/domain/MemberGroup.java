1a8d497472dc25b7ee5c582a6ef377af38f406ef
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-05-08 01:45:15 +0900

    Feat: MemberGroup 컬럼 제약조건 변경
    - columnDefinition = "default 'fb392c'"

--- backend/src/main/java/com/butter/wypl/group/domain/MemberGroup.java
+++ backend/src/main/java/com/butter/wypl/group/domain/MemberGroup.java
@@ -2,23 +2,28 @@ package com.butter.wypl.group.domain;
 
 import com.butter.wypl.global.common.BaseEntity;
 import com.butter.wypl.member.domain.Member;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 @IdClass(MemberGroupId.class)
 public class MemberGroup extends BaseEntity {
 
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
+	@Id
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "group_id")
-    private Group group;
+	@Id
+	@ManyToOne
+	@JoinColumn(name = "group_id")
+	private Group group;
 
-    @Column(name = "color", nullable = false, length = 6)
-    private String coler;
-
+	@Column(name = "color", length = 6, columnDefinition = "default 'fb392c'")
+	private String color; // 기본 값: 오렌지 색
 }
