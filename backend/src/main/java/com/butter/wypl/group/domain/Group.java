9a0e9fc5363643217ee9122a9bfe3097afb2f2d3
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-05-07 17:37:17 +0900

    Feat: Group Entity of 메서드 추가
    - Group Entity of 메서드 추가

--- backend/src/main/java/com/butter/wypl/group/domain/Group.java
+++ backend/src/main/java/com/butter/wypl/group/domain/Group.java
@@ -1,36 +1,56 @@
 package com.butter.wypl.group.domain;
 
+import java.util.List;
+
 import com.butter.wypl.global.common.BaseEntity;
 import com.butter.wypl.member.domain.Member;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "groups")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Group extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "group_id")
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "group_id")
+	private int id;
 
-    @Column(name = "name", length = 20, nullable = false)
-    private String name;
+	@Column(name = "name", length = 20, nullable = false)
+	private String name;
 
-    @Column(name = "description", length = 50)
-    private String description;
+	@Column(name = "description", length = 50)
+	private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "owner_id")
-    private Member ownerId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner_id")
+	private Member owner;
 
-    @Builder
-    public Group(String name, String description, Member ownerId) {
-        this.name = name;
-        this.description = description;
-        this.ownerId = ownerId;
-    }
+	@OneToMany(mappedBy = "group")
+	private List<MemberGroup> memberGroups;
+
+	public Group(String name, String description, Member owner) {
+		this.name = name;
+		this.description = description;
+		this.owner = owner;
+	}
+
+	public static Group of(String name, String description, Member owner) {
+		return new Group(name, description, owner);
+	}
+
 }
