598196f3e4205ef278ba766e45ca494124368802
좌소연 <thdus981005@naver.com>
2024-05-07 10:41:52 +0900

    Feat : 캘린더 관련 schedule repository 및 service 기능 추가 구현(S10P31A602-402)

--- backend/src/main/java/com/butter/wypl/schedule/respository/ScheduleRepository.java
+++ backend/src/main/java/com/butter/wypl/schedule/respository/ScheduleRepository.java
@@ -18,5 +18,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
 
 	//해당 반복에 대해 이후 일정 찾기
 	List<Schedule> findAllByRepetitionAndStartDateAfter(Repetition repetition, LocalDateTime startDate);
+
+	List<Schedule> findAllByStartDateBetween(LocalDateTime firstDate, LocalDateTime endDate);
 }
 
--- backend/src/main/java/com/butter/wypl/schedule/utils/ScheduleServiceUtils.java
+++ backend/src/main/java/com/butter/wypl/schedule/utils/ScheduleServiceUtils.java
@@ -1,5 +1,8 @@
 package com.butter.wypl.schedule.utils;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.butter.wypl.schedule.domain.Schedule;
 import com.butter.wypl.schedule.exception.ScheduleErrorCode;
 import com.butter.wypl.schedule.exception.ScheduleException;
@@ -13,5 +16,13 @@ public class ScheduleServiceUtils {
 	) {
 		return scheduleRepository.findById(id)
 			.orElseThrow(() -> new ScheduleException(ScheduleErrorCode.NO_SCHEDULE));
+	}
+
+	public static List<Schedule> findSchedulesBetween(
+		final ScheduleRepository scheduleRepository,
+		final LocalDateTime firstDate,
+		final LocalDateTime endDate
+	) {
+		return scheduleRepository.findAllByStartDateBetween(firstDate, endDate);
 	}
 }
