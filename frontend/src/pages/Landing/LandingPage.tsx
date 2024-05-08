4f702a35dcf003a7b8ac0d57102384982318bfdd
KIMSEI1124 <workju1124@gmail.com>
2024-05-08 17:08:52 +0900

    Design: 구글 로그인 버튼 구현 (S10P31A602-238)

--- frontend/src/pages/Landing/LandingPage.tsx
+++ frontend/src/pages/Landing/LandingPage.tsx
@@ -1,7 +1,11 @@
+import * as S from './LandingPage.styled';
+
 function LandingPage() {
   return (
-    <div>LandingPage</div>
-  )
+    <S.Container>
+      <S.GoogleButton>Sign in with Google</S.GoogleButton>
+    </S.Container>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
