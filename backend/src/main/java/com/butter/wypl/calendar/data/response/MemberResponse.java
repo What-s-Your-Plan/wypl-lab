77f70de0979302333173a72ab027956d7b1cc687
KIMSEI1124 <workju1124@gmail.com>
2024-05-08 13:31:49 +0900

    Hotfix: 컴파일 에러 수정

--- backend/src/main/java/com/butter/wypl/calendar/data/response/MemberResponse.java
+++ backend/src/main/java/com/butter/wypl/calendar/data/response/MemberResponse.java
@@ -1,23 +1,24 @@
 package com.butter.wypl.calendar.data.response;
 
+import com.butter.wypl.global.common.Color;
 import com.butter.wypl.member.domain.Member;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record MemberResponse(
 
-	@JsonProperty("member_id")
-	int memberId,
+		@JsonProperty("member_id")
+		int memberId,
 
-	String nickname,
+		String nickname,
 
-	String color //TODO : pull 받고 Color로 수정해야 됨
+		Color color //TODO : pull 받고 Color로 수정해야 됨
 ) {
 
 	public static MemberResponse from(Member member) {
 		return new MemberResponse(
-			member.getId(),
-			member.getNickname(),
-			member.getColor()
+				member.getId(),
+				member.getNickname(),
+				member.getColor()
 		);
 	}
 }
