d6f1c1fd82c2517d08feead55d63b56479cbfb09
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-05-09 13:31:56 +0900

    Refactor: Delete MemberGroupService getMembersByGroupId method

--- backend/src/main/java/com/butter/wypl/group/service/MemberGroupService.java
+++ backend/src/main/java/com/butter/wypl/group/service/MemberGroupService.java
@@ -1,13 +1,7 @@
 package com.butter.wypl.group.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.butter.wypl.group.domain.MemberGroup;
-import com.butter.wypl.group.repository.MemberGroupRepository;
-import com.butter.wypl.schedule.data.response.MemberResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,15 +9,4 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberGroupService {
-
-	private final MemberGroupRepository memberGroupRepository;
-
-	public List<MemberResponse> getMembersByGroupId(int groupId) {
-		List<MemberGroup> memberGroups = memberGroupRepository.findMemberGroupsByGroupId(groupId);
-		return memberGroups.stream()
-			.map(MemberGroup::getMember)
-			.map(MemberResponse::from)
-			.toList();
-	}
-
 }
