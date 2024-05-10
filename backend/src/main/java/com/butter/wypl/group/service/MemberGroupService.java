f471bb7bd3be44a98d1e555c317747388f5d376a
한지원 <88698607+jiwon83@users.noreply.github.com>
2024-05-10 14:02:11 +0900

    Feat: 그룹 삭제 서비스

--- backend/src/main/java/com/butter/wypl/group/service/MemberGroupService.java
+++ backend/src/main/java/com/butter/wypl/group/service/MemberGroupService.java
@@ -3,7 +3,6 @@ package com.butter.wypl.group.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.butter.wypl.group.domain.MemberGroup;
 import com.butter.wypl.group.repository.MemberGroupRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,9 +14,4 @@ public class MemberGroupService {
 
 	private final MemberGroupRepository memberGroupRepository;
 
-	@Transactional
-	public void deleteMemberGroup(int memberId, int groupId) {
-		MemberGroup memberGroup = memberGroupRepository.findMemberGroupByMemberIdAndGroupId(memberId, groupId);
-		memberGroup.delete();
-	}
 }
