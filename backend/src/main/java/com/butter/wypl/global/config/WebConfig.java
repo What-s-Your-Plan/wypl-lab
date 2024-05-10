742d975b7d4968089c85a3d26ad4563e8cf995cb
KIMSEI1124 <workju1124@gmail.com>
2024-05-10 09:58:09 +0900

    Feat: Http Options 추가 (S10P31A602-465)

--- backend/src/main/java/com/butter/wypl/global/config/WebConfig.java
+++ backend/src/main/java/com/butter/wypl/global/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "FETCH")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "FETCH", "OPTIONS")
 				.allowedOrigins("*")
 				.maxAge(5000);
 	}
