d970c77fb3d6111aeb9e24ce27358f9c05a45aa0
KIMSEI1124 <workju1124@gmail.com>
2024-05-10 13:29:48 +0900

    Refactor: 컴포넌트 배열 처리 (S10P31A602-250)

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,19 +2,25 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import logo from '@/assets/images/logo.png';
-import NavEventBar from '@/components/navbar/NavEventBar/NavEventBar';
+
 import MemberSheet from '@/components/navbar/sheet/member/MemberSheet';
+import NavEventBar from '@/components/navbar/NavEventBar/NavEventBar';
+import NotificationSheet from '@/components/navbar/sheet/notification/NotificationSheet';
+import Sheet from '@/components/navbar/sheet/Sheet';
+
 import { BROWSER_PATH } from '@/constants/Path';
 
 import * as S from './Navbar.styled';
-import NotificationSheet from './sheet/notification/NotificationSheet';
-import Sheet from './sheet/Sheet';
+
+type SheetComponent = {
+  sheetType: SheetType;
+  component: JSX.Element;
+};
 
 function Navbar() {
   const navigate = useNavigate();
 
   const [sheet, setSheet] = useState<SheetType>('NONE');
-
   const changeSheet = (newSheet: SheetType) => {
     if (sheet === newSheet) {
       resetSheet();
@@ -22,10 +28,20 @@ function Navbar() {
     }
     setSheet(newSheet);
   };
-
   const resetSheet = () => {
     setSheet('NONE');
   };
+
+  const sheetComponents: SheetComponent[] = [
+    {
+      sheetType: 'MEMBER',
+      component: <MemberSheet />,
+    },
+    {
+      sheetType: 'NOTIFICATION',
+      component: <NotificationSheet />,
+    },
+  ];
 
   return (
     <S.Container>
@@ -36,17 +52,15 @@ function Navbar() {
         ></S.Logo>
         <NavEventBar changeSheetEvent={changeSheet} />
       </S.Wrapper>
-      {sheet === 'MEMBER' && (
-        <Sheet
-          resetSheetEvent={resetSheet}
-          childrenComponent={<MemberSheet />}
-        />
-      )}
-      {sheet === 'NOTIFICATION' && (
-        <Sheet
-          resetSheetEvent={resetSheet}
-          childrenComponent={<NotificationSheet />}
-        />
+      {sheetComponents.map(
+        (sheetComponent: SheetComponent) =>
+          sheet === sheetComponent.sheetType && (
+            <Sheet
+              key={sheetComponent.sheetType}
+              resetSheetEvent={resetSheet}
+              childrenComponent={sheetComponent.component}
+            />
+          ),
       )}
     </S.Container>
   );
