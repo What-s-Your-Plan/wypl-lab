8b6e25e4a7c115c28b623f670835c4bf5b4cd041
KIMSEI1124 <workju1124@gmail.com>
2024-05-12 17:13:39 +0900

    Fix: DB 수정 (S10P31A602-494)

--- backend/src/main/java/com/butter/wypl/group/repository/MemberGroupRepository.java
+++ backend/src/main/java/com/butter/wypl/group/repository/MemberGroupRepository.java
@@ -1,6 +1,5 @@
 package com.butter.wypl.group.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,17 +7,29 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.butter.wypl.group.domain.MemberGroup;
 import com.butter.wypl.group.domain.MemberGroupId;
+import com.butter.wypl.group.repository.query.MemberGroupRepositoryCustom;
 
-public interface MemberGroupRepository extends JpaRepository<MemberGroup, MemberGroupId> {
+public interface MemberGroupRepository extends JpaRepository<MemberGroup, MemberGroupId>,
+		MemberGroupRepositoryCustom {
 
 	@Query("SELECT COUNT(mg) FROM MemberGroup mg WHERE mg.member.id = :memberId")
 	int countByMemberId(int memberId);
 
-	@Query("SELECT mg FROM MemberGroup mg JOIN FETCH mg.member m JOIN FETCH mg.group g WHERE mg.group.id = :groupId and mg.groupInviteState = 'ACCEPTED'")
+	/* TODO: QueryDSL 마이그레이션
+	@Query("SELECT mg FROM MemberGroup mg "
+			+ "JOIN FETCH mg.member m "
+			+ "JOIN FETCH mg.group g "
+			+ "WHERE mg.group.id = :groupId and mg.groupInviteState = 'ACCEPTED'")
 	List<MemberGroup> findMemberGroupsByGroupId(int groupId);
 
-	@Query("SELECT mg FROM MemberGroup mg JOIN FETCH mg.member m JOIN FETCH mg.group g WHERE mg.group.id = :groupId and mg.member.id = :memberId and mg.groupInviteState = 'PENDING'")
+	@Query("SELECT mg FROM MemberGroup mg "
+			+ "JOIN FETCH mg.member m "
+			+ "JOIN FETCH mg.group g "
+			+ "WHERE mg.group.id = :groupId "
+			+ "and mg.member.id = :memberId "
+			+ "and mg.groupInviteState = 'PENDING'")
 	Optional<MemberGroup> findFirstPendingMemberGroupsByGroupId(int memberId, int groupId);
+	*/
 
 	Optional<MemberGroup> findMemberGroupByMemberIdAndGroupId(int memberId, int groupId);
 
---
+++ backend/src/main/java/com/butter/wypl/group/repository/query/MemberGroupRepositoryCustomImpl.java
@@ -0,0 +1,44 @@
+package com.butter.wypl.group.repository.query;
+
+import static com.butter.wypl.group.domain.QGroup.*;
+import static com.butter.wypl.group.domain.QMemberGroup.*;
+import static com.butter.wypl.member.domain.QMember.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.butter.wypl.group.domain.GroupInviteState;
+import com.butter.wypl.group.domain.MemberGroup;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberGroupRepositoryCustomImpl implements MemberGroupRepositoryCustom {
+
+	private final JPAQueryFactory query;
+
+	@Override
+	public Optional<MemberGroup> findFirstPendingMemberGroupsByGroupId(int memberId, int groupId) {
+		MemberGroup findMemberGroup = query.selectFrom(memberGroup)
+				.join(memberGroup.member, member).fetchJoin()
+				.join(memberGroup.group, group).fetchJoin()
+				.where(group.id.eq(groupId)
+						.and(member.id.eq(memberId))
+						.and(memberGroup.groupInviteState.eq(GroupInviteState.PENDING))
+						.and(member.deletedAt.isNull()))
+				.fetchFirst();
+		return Optional.ofNullable(findMemberGroup);
+	}
+
+	@Override
+	public List<MemberGroup> findMemberGroupsByGroupId(int groupId) {
+		return query.selectFrom(memberGroup)
+				.join(memberGroup.member, member).fetchJoin()
+				.join(memberGroup.group, group).fetchJoin()
+				.where(group.id.eq(groupId)
+						.and(memberGroup.groupInviteState.eq(GroupInviteState.ACCEPTED))
+						.and(member.deletedAt.isNull()))
+				.fetch();
+	}
+}
