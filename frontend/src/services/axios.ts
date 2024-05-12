a8d762c8c09d8690f70e6b20416d6fabc588d8a1
KIMSEI1124 <workju1124@gmail.com>
2024-05-12 20:05:15 +0900

    Feat: Reissue 기능 구현 (S10P31A602-469)

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,6 +1,7 @@
-import Axios from 'axios';
+import Axios, { HttpStatusCode } from 'axios';
 
 import useJsonWebTokensStore from '@/stores/TokenStore';
+import reissueTokens from './auth/reissue';
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
@@ -33,12 +34,54 @@ axiosWithAccessToken.interceptors.request.use((config) => {
   return config;
 });
 
+axiosWithAccessToken.interceptors.response.use(null, async function (error) {
+  const refreshToken = useJsonWebTokensStore.getState().refreshToken;
+  if (error.response.status === HttpStatusCode.Unauthorized && refreshToken) {
+    const params: ReissueTokenParams = {
+      refresh_token: refreshToken,
+    };
+    const body = await reissueTokens(params);
+    if (body === null) {
+      return Promise.reject(error);
+    }
+    useJsonWebTokensStore.getState().setAccessToken(body.access_token);
+    useJsonWebTokensStore.getState().setRefreshToken(body.refresh_token);
+    error.config.headers.Authorization = `Bearer ${useJsonWebTokensStore.getState().accessToken}`;
+    return axiosWithAccessToken(error.config);
+  }
+  if (error.response.status === HttpStatusCode.Forbidden) {
+    useJsonWebTokensStore.getState().resetTokens();
+  }
+  return Promise.reject(error);
+});
+
 axiosWithMultiPart.interceptors.request.use((config) => {
   const accessToken = useJsonWebTokensStore.getState().accessToken;
   if (config.headers && accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
   return config;
+});
+
+axiosWithMultiPart.interceptors.response.use(null, async function (error) {
+  const refreshToken = useJsonWebTokensStore.getState().refreshToken;
+  if (error.response.status === HttpStatusCode.Unauthorized && refreshToken) {
+    const params: ReissueTokenParams = {
+      refresh_token: refreshToken,
+    };
+    const body = await reissueTokens(params);
+    if (body === null) {
+      return Promise.reject(error);
+    }
+    useJsonWebTokensStore.getState().setAccessToken(body.access_token);
+    useJsonWebTokensStore.getState().setRefreshToken(body.refresh_token);
+    error.config.headers.Authorization = `Bearer ${useJsonWebTokensStore.getState().accessToken}`;
+    return axiosWithMultiPart(error.config);
+  }
+  if (error.response.status === HttpStatusCode.Forbidden) {
+    useJsonWebTokensStore.getState().resetTokens();
+  }
+  return Promise.reject(error);
 });
 
 export { baseURL, axios, axiosWithAccessToken, axiosWithMultiPart };
