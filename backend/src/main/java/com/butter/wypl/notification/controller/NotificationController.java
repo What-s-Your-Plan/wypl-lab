35aebbfff10d7908ed559ad9278ecdcf18b812cb
최민준 <dailymaro@naver.com>
2024-05-13 15:49:47 +0900

    Fix : 알림 조회 PathVariable -> RequestParam 변경

--- backend/src/main/java/com/butter/wypl/notification/controller/NotificationController.java
+++ backend/src/main/java/com/butter/wypl/notification/controller/NotificationController.java
@@ -5,9 +5,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -61,10 +61,10 @@ public class NotificationController {
 			.body(emitter);
 	}
 
-	@GetMapping("/{lastId}")
+	@GetMapping
 	public ResponseEntity<Message<NotificationPageResponse>> getNotificationsPage(
 		@Authenticated AuthMember authMember,
-		@PathVariable(name = "lastId", required = false) String lastId
+		@RequestParam(name = "lastId", required = false) String lastId
 	) {
 		return ResponseEntity.ok()
 			.body(Message.withBody("알림 조회 성공", notificationLoadService.getNotifications(authMember.getId(), lastId)));
