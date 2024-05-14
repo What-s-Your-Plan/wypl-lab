ebd62456205de9a7cd094e956948762287e2e3f0
KIMSEI1124 <workju1124@gmail.com>
2024-05-14 21:53:08 +0900

    Remove: useEffect 제거

--- frontend/src/components/widget/WidgetList.tsx
+++ frontend/src/components/widget/WidgetList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
 import * as S from '@/components/common/Container';
