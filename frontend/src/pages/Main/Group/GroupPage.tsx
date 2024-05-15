7a8cca7020b96b83bb7e63c1b39452b7cfcb54cf
KIMSEI1124 <workju1124@gmail.com>
2024-05-16 03:01:12 +0900

    Design: 요소가 없을 경우 Lottie 출력 (S10P31A602-590)

--- frontend/src/pages/Main/Group/GroupPage.tsx
+++ frontend/src/pages/Main/Group/GroupPage.tsx
@@ -1,6 +1,11 @@
-import GroupCalendar from '@/components/group/GroupCalendar';
-import GroupList from '@/components/group/GroupList';
 import { useParams } from 'react-router-dom';
+
+// import GroupCalendar from '@/components/group/GroupCalendar';
+import GroupList from '@/components/group/GroupList';
+
+import CalendarContent from '@/components/calendar/CalendarContent';
+import NoContentAnimation from '@/components/animation/NoContent';
+import { Container } from '@/components/common/Container';
 
 function GroupPage() {
   const { groupId } = useParams();
@@ -8,7 +13,14 @@ function GroupPage() {
   return (
     <div className="container flex items-center justify-around ss:max-sm:block h-dvh">
       <GroupList />
-      <GroupCalendar groupId={Number(groupId)} />
+      {/* <GroupCalendar groupId={Number(groupId)} /> */}
+      {groupId ? (
+        <CalendarContent />
+      ) : (
+        <Container $width={'right'}>
+          <NoContentAnimation />
+        </Container>
+      )}
     </div>
   );
 }
