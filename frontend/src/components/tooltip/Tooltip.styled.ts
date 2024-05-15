c06a021de7e2a027aec17bccc38a1acd3daaf787
KIMSEI1124 <workju1124@gmail.com>
2024-05-16 03:00:22 +0900

    Design: 마우스 이벤트 막음 (S10P31A602-590)

--- frontend/src/components/tooltip/Tooltip.styled.ts
+++ frontend/src/components/tooltip/Tooltip.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Text = styled.span.attrs({
   className:
-    'absolute bg-default-black text-default-white text-xs rounded py-1 px-2 z-50 opacity-0',
+    'absolute bg-default-black text-default-white text-xs rounded py-1 px-2 opacity-0',
 })`
   width: max-content;
   bottom: 100%;
@@ -10,9 +10,11 @@ const Text = styled.span.attrs({
   transform: translateX(-50%);
   margin-bottom: 5px;
   transition: opacity 0.3s;
+  pointer-events: none;
 
   &:hover {
     opacity: 1;
+    pointer-events: auto;
   }
 `;
 
