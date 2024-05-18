44ec3ab2cc83c31a492f2d5a5c3ccfd75bf4e87b
Jiwon <leeji7031@gmail.com>
2024-05-19 02:37:51 +0900

    Fix: 그룹 랜더링 수정(S10P31A602-607)

--- frontend/src/pages/Main/Group/GroupPage.tsx
+++ frontend/src/pages/Main/Group/GroupPage.tsx
@@ -2,19 +2,14 @@ import { useParams } from 'react-router-dom';
 
 import GroupList from '@/components/group/GroupList';
 import CalendarContent from '@/components/calendar/CalendarContent';
-import { useEffect, useState } from 'react';
 
 function GroupPage() {
-  const { groupId } = useParams();
-  const [id, setId] = useState<number | undefined>(undefined);
-  useEffect(() => {
-    setId(Number(groupId));
-  }, [groupId]);
+  let { groupId } = useParams();
 
   return (
     <div className="container flex items-center justify-around ss:max-sm:block h-dvh">
       <GroupList />
-      <CalendarContent category="GROUP" groupId={id} />
+      <CalendarContent category="GROUP" groupId={Number(groupId)} />
     </div>
   );
 }
