b7667d90e2909a44411503bd901fbc19a41026c4
조다민 <jdm080620@gmail.com>
2024-05-20 03:39:47 +0900

    Fix: 사진 사이즈 수정

--- frontend/src/components/review/view/VPicture.tsx
+++ frontend/src/components/review/view/VPicture.tsx
@@ -8,7 +8,7 @@ type VPictureProps = {
 function VPicture({ content }: VPictureProps) {
   return (
     <WhiteContainer $width="900" className="flex justify-center">
-      <img src={content.path} alt="사진" />
+      <img src={content.path} alt="사진" className='w-80' />
     </WhiteContainer>
   );
 }
