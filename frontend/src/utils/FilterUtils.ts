efee1d19d7c3e7920be224246f9017bc53d9c815
Jiwon <leeji7031@gmail.com>
2024-05-20 03:45:09 +0900

    Fix: filter

--- frontend/src/utils/FilterUtils.ts
+++ frontend/src/utils/FilterUtils.ts
@@ -6,13 +6,13 @@ function labelFilter(schedules: CalendarSchedule[], labels: FilterResponse[]) {
   return schedules.filter((schedule) => {
     if (schedule.label) {
       for (let i = 0; i < labels.length; i++) {
-        if (labels[i].id === schedule.label.label_id) {
+        if (labels[i].id === schedule.label.label_id && labels[i].category === 'MEMBER') {
           return true;
         }
       }
     } else if (schedule.group) {
       for (let i = 0; i < labels.length; i++) {
-        if (labels[i].id === schedule.group.group_id) {
+        if (labels[i].id === schedule.group.group_id && labels[i].category === 'GROUP') {
           return true;
         }
       }
